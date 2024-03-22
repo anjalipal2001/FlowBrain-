6cdc769bc6e26cd046078381b52f5fce11e58c45
anjalipal2001 <ap7197339@gmail.com>
2024-03-23 01:51:32 +0530

    Added Notes Layout

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,11 +1,12 @@
-import { Metadata } from "next";
-
+import Note from "@/components/Note";
 import prisma from "@/lib/db/prisma";
 import { auth } from "@clerk/nextjs";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "FlowBrain - AI Notes App",
+  title: "FlowBrain - Notes",
 };
+
 export default async function NotesPage() {
   const { userId } = auth();
 
@@ -13,5 +14,16 @@ export default async function NotesPage() {
 
   const allNotes = await prisma.note.findMany({ where: { userId } });
 
-  return <div>{JSON.stringify(allNotes)}</div>;
+  return (
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+      {allNotes.map((note) => (
+        <Note note={note} key={note.id} />
+      ))}
+      {allNotes.length === 0 && (
+        <div className="col-span-full text-center">
+          {"You don't have any notes yet. Why don't you create one?"}
+        </div>
+      )}
+    </div>
+  );
 }
